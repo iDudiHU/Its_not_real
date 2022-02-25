88e04b040b12d9639a5cc3155adf414701b50451
QDWAZAR <matyasdudas97@gmail.com>
2022-02-24 22:52:07 -0800

    Added corridor, health pick up, creeping terror

---
+++ Assets/Darkness_Meter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Darkness_Meter : MonoBehaviour
+{
+    public GameObject healthbar;
+    public Slider slider;
+    public Image image;
+    public float presetAlpha;
+    void Start()
+    {
+        image = GetComponent<Image>();
+        slider = healthbar.GetComponent<Slider>();
+    }
+
+	private void Update()
+	{
+        var newAlpha = image.color;
+		newAlpha.a = 1 - (slider.value/100 - presetAlpha);
+        image.color = newAlpha;
+	}
+}
